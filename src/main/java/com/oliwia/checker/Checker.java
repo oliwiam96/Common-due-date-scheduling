a24f6ff57e3cd8625c9e59a9c5c17625ce220051
Oliwia <oliwia.m.96@wp.pl>
2018-10-27 23:27:41 +0200

    Add executing batch file

--- src/main/java/com/oliwia/checker/Checker.java
+++ src/main/java/com/oliwia/checker/Checker.java
@@ -3,37 +3,50 @@ package com.oliwia.checker;
 import com.oliwia.common.FileReaderWriter;
 import com.oliwia.solver.SubProblem;
 
+import java.io.IOException;
+
 public class Checker {
     public static void main(String[] args) {
         System.out.println("Checker by Oliwia Masian 127324");
 
         String inputBatName = args[0];
-        String studentIndex = inputBatName.substring(0, inputBatName.length()-4);
+        String studentIndex = inputBatName.substring(0, inputBatName.length() - 4);
         System.out.println(studentIndex);
         int inputN = Integer.parseInt(args[1]);
         int inputK = Integer.parseInt(args[2]);
         double inputH = Double.parseDouble(args[3]);
-
-        // run bat and measure time
-
-        SubProblem subProblem = FileReaderWriter.getKthProblemWithH(inputK, inputN, inputH);
-        StringBuilder sb = new StringBuilder();
-        sb.append("sch_").append(studentIndex).append("_")
-                .append(inputN).append("_")
-                .append(inputK).append("_")
-                .append((int) (inputH*10)).append(".out");
-        String fileNameToOpen = sb.toString();
-        int declaredCost = FileReaderWriter.getDeclaredCostAndSetOrder(subProblem, fileNameToOpen);
-        System.out.println("Declared cost: " + declaredCost + " calculated cost: " + subProblem.getCostFunction());
-        if(declaredCost != subProblem.getCostFunction()){
-            System.out.println("ERROR! COSTS ARE DIFFERENT!");
-        } else{
-            System.out.println("Costs are equal");
+        StringBuilder toExecuteString = new StringBuilder();
+        toExecuteString.append(inputBatName).append(" ")
+                .append(inputN).append(" ")
+                .append(inputK).append(" ")
+                .append(inputH);
+        try {
+            long startTime = System.currentTimeMillis();
+            Runtime
+                    .getRuntime()
+                    .exec(toExecuteString.toString())
+                    .waitFor();
+            long stopTime = System.currentTimeMillis();
+            long elapsedTime = stopTime - startTime;
+            System.out.println("Elapsed time: " + elapsedTime + " ms");
+            SubProblem subProblem = FileReaderWriter.getKthProblemWithH(inputK, inputN, inputH);
+            StringBuilder outputFileName = new StringBuilder();
+            outputFileName.append("sch_").append(studentIndex).append("_")
+                    .append(inputN).append("_")
+                    .append(inputK).append("_")
+                    .append((int) (inputH * 10)).append(".out");
+            String fileNameToOpen = outputFileName.toString();
+            int declaredCost = FileReaderWriter.getDeclaredCostAndSetOrder(subProblem, fileNameToOpen);
+            System.out.println("Declared cost: " + declaredCost + "; calculated cost: " + subProblem.getCostFunction());
+            if (declaredCost != subProblem.getCostFunction()) {
+                System.out.println("ERROR! COSTS ARE DIFFERENT!");
+            } else {
+                System.out.println("Costs are equal.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-
-    }
-    private void check(){
-
     }
 }
